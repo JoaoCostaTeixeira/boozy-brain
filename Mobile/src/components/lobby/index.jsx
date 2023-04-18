74461abf429ec9f806525826bc7089ed1b4152f7
Debora-dinis <debora.dinis99@gmail.com>
2023-04-18 11:04:56 +0100

    debora changes

--- Mobile/src/components/lobby/index.jsx
+++ Mobile/src/components/lobby/index.jsx
@@ -14,8 +14,8 @@ function Login({ sendUserName }) {
     <div className="Main">
       <div className="connectDIV">
       <div className="gameTitle">
-        <span className="gameTitlesSpan"><Font family='Geo'>Boozy Brain</Font></span>
-        <span className="gameTitlesSpan2"><Font family='Geo'>Can you outsmart the bottle?</Font></span>
+        <span className="gameTitlesSpan"><Font family='VT323'>Boozy Brain</Font></span>
+        <span className="gameTitlesSpan2"><Font family='VT323'>Can you outsmart the bottle?</Font></span>
       </div>
         <input
           onFocus={() => document.body.requestFullscreen()}
@@ -23,7 +23,7 @@ function Login({ sendUserName }) {
           onKeyUp={(e) => (e.target.value = e.target.value.toUpperCase())}
         />
         <button className="connect" onClick={send}>
-          <Font family='Geo'>START</Font>
+          <Font family='VT323'>START</Font>
         </button>
       </div>
     </div>
