f0377c1ec8b68a03348b573d2d9ae161101019bf
Debora-dinis <debora.dinis99@gmail.com>
2023-04-22 14:58:34 +0100

    change styling time bar

--- Desktop-front-end/src/components/utils/TimerBar.jsx
+++ Desktop-front-end/src/components/utils/TimerBar.jsx
@@ -3,17 +3,24 @@ import styled from "styled-components";
 
 const TimerStyle = styled.div`
 width: 80vw;
-height: 1vw;
-position: absolute;
-bottom: 4vh;
+    height: 3.5vh;
+    position: absolute;
+    bottom: 4vh;
+    display: flex;
+    background: #ff0000;
+    border: 5px solid #5f0404;
+    box-shadow: 0px 0px 8px 0px #151212f7 inset;
+    border-radius: 0.8vw;
 }
 `;
 
 const TimerInnerDivStyle = styled.div`
 height: 20px;
-background-color: #ffe700;
+background: linear-gradient(180deg, rgba(255,198,4,1) 9%, rgba(254,255,0,1) 27%, rgba(250,227,109,1) 41%, rgba(247,215,112,1) 51%, rgba(254,255,0,1) 61%, rgba(255,198,4,1) 82%);
 transition: all 1s linear 0s;
-border-radius: 1vh;
+border-radius: 0.8vh;
+border: solid rgb(180, 149, 16) 2px;
+margin-block: auto;
 }
 `;
 
