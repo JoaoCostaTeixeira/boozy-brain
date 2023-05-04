11431923a6ded661857943f8d7426ef0cc506b30
Debora-dinis <debora.dinis99@gmail.com>
2023-05-04 18:16:06 +0100

    updated  questions databases

---
+++ Desktop-back-end/src/database/normal/index.js
@@ -0,0 +1,93 @@
+let db = null;
+let categoryPercentages = {};
+
+const category = [
+    "Science",
+    "Entertainment",
+    "Art_History",
+    "Sports",
+    "Geography",
+    "Languages",
+];
+
+async function createDB() {
+    const { JsonDB, Config } = require("node-json-db");
+
+    db = new JsonDB(
+        new Config("./src/database/normal/db", true, false, "/")
+    );
+    // Normal question consts assign
+    let totalCount = 0;
+    for (const cat of category) {
+        const count = await db.count(`/${cat}`);
+        categoryPercentages[cat] = { count };
+        totalCount += count;
+    }
+    for (const cat of category) {
+        categoryPercentages[cat]["percentage"] =
+            (categoryPercentages[cat].count / totalCount) * 100;
+    }
+}
+
+// GET numQuestion questions from DB
+async function getQuestions(numQuestions) {
+    const questions = [];
+    while (questions.length < numQuestions) {
+        const cat = Math.random() * 100;
+        let cat2;
+
+        if (cat < categoryPercentages.Science.percentage) {
+            cat2 = 0;
+        } else if (
+            cat <
+            categoryPercentages.Science.percentage +
+            categoryPercentages.Entertainment.percentage
+        ) {
+            cat2 = 1;
+        } else if (
+            cat <
+            categoryPercentages.Science.percentage +
+            categoryPercentages.Entertainment.percentage +
+            categoryPercentages.Art_History.percentage
+        ) {
+            cat2 = 2;
+        } else if (
+            cat <
+            categoryPercentages.Science.percentage +
+            categoryPercentages.Entertainment.percentage +
+            categoryPercentages.Art_History.percentage +
+            categoryPercentages.Sports.percentage
+        ) {
+            cat2 = 3;
+        } else if (
+            cat <
+            categoryPercentages.Science.percentage +
+            categoryPercentages.Entertainment.percentage +
+            categoryPercentages.Art_History.percentage +
+            categoryPercentages.Sports +
+            categoryPercentages.Geography.percentage
+        ) {
+            cat2 = 4;
+        } else {
+            cat2 = 5;
+        }
+
+        const { count } = categoryPercentages[category[cat2]];
+        if (!count) {
+            continue;
+        }
+        const question = Math.floor(Math.random() * count);
+        const data = await db.getData(`/${category[cat2]}[${question}]`);
+        if (
+            !questions.find(
+                (question) => question["question_id"] === data["question_id"]
+            )
+        )
+            questions.push({ ...data, type: "normal" });
+    }
+
+    return questions;
+}
+
+
+module.exports = { createDB, getQuestions };
---
+++ Desktop-back-end/src/database/reverse/index.js
@@ -0,0 +1,19 @@
+let db = null;
+
+async function createDB() {
+    const { JsonDB, Config } = require("node-json-db");
+
+    db = new JsonDB(
+        new Config("./src/database/reverse/db", true, false, "/")
+    );
+
+}
+
+// GET numQuestion questions from DB
+async function getQuestions(numQuestions) {
+    const questions = [];
+    return questions;
+}
+
+
+module.exports = { createDB, getQuestions };
